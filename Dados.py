96c2c11077d7a7d8dbc8b1557d416bf637e7703b
Marcos <marcoswillireiscampos@gmail.com>
2018-11-06 23:43:30 -0200

    Criação do índice de criminalidade e sua plotagem

--- Dados.py
+++ Dados.py
@@ -53,11 +53,11 @@ populacao_municipal = populacao_municipal[2019 > populacao_municipal.vano]
 Data = []
 for i in list(range(1991, 2018)):
     for j in list(range(1, 10)):
-        Data.append('{}/0{}'.format(i, j))
+        Data.append('0{}/{}'.format(j, i))
     for j in list(range(10, 13)):
-        Data.append('{}/{}'.format(i, j))
+        Data.append('{}/{}'.format(j, i))
 for j in list(range(1, 9)):
-    Data.append('2018/0{}'.format(j))
+    Data.append('0{}/2018'.format(j))
 Data = np.array(Data).reshape(332,1)
     
 se = np.array(serie_estadual)
@@ -68,11 +68,11 @@ serie_estadual.columns = ['Data', 'Ano', 'Mês', 'Homicídio Doloso', 'Lesão Co
 
 se_ano = serie_estadual[:]
 se_ano.index = [se_ano['Mês']]
-grupo_ano_se = se_ano.groupby(se_ano['Ano'])
+grupo_ano_se = se_ano.groupby('Ano')
 
 se_mes = serie_estadual[:]
 se_mes.index = [se_mes['Ano']]
-grupo_mes_se = se_mes.groupby(se_mes['Mês'])
+grupo_mes_se = se_mes.groupby('Mês')
 
 serie_estadual.index = serie_estadual['Data']
 serie_estadual = serie_estadual.drop('Data', axis=1)
@@ -80,11 +80,11 @@ serie_estadual = serie_estadual.drop('Data', axis=1)
 Data = []
 for i in list(range(2003, 2018)):
     for j in list(range(1, 10)):
-        Data.append('{}/0{}'.format(i, j))
+        Data.append('0{}/{}'.format(j, i))
     for j in list(range(10, 13)):
-        Data.append('{}/{}'.format(i, j))
+        Data.append('{}/{}'.format(j, i))
 for j in list(range(1, 9)):
-    Data.append('2018/0{}'.format(j))
+    Data.append('0{}/2018'.format(j))
 Data = np.array(Data).reshape(188,1)
     
 se_t = np.array(serie_estadual_taxas)
@@ -95,11 +95,11 @@ serie_estadual_taxas.columns = ['Data', 'Ano', 'Mês', 'Homicídio Doloso', 'Les
 
 set_ano = serie_estadual_taxas[:]
 set_ano.index = [set_ano['Mês']]
-grupo_ano_set = set_ano.groupby(set_ano['Ano'])
+grupo_ano_set = set_ano.groupby('Ano')
 
 set_mes = serie_estadual_taxas[:]
 set_mes.index = [set_mes['Ano']]
-grupo_mes_set = set_mes.groupby(set_mes['Mês'])
+grupo_mes_set = set_mes.groupby('Mês')
 
 serie_estadual_taxas.index = serie_estadual_taxas['Data']
 serie_estadual_taxas = serie_estadual_taxas.drop('Data', axis=1)
@@ -110,17 +110,17 @@ for i in list(range(2014, 2018)):
     for j in list(range(1, 10)):
         
         for t in list(range(92)):
-            Data.append('{}/0{}'.format(i, j))
+            Data.append('0{}/{}'.format(j, i))
         
     for j in list(range(10, 13)):
         
         for t in list(range(92)):
-            Data.append('{}/{}'.format(i, j))
+            Data.append('{}/{}'.format(j, i))
         
 for j in list(range(1, 9)):
     
     for t in list(range(92)):
-        Data.append('2018/0{}'.format(j))
+        Data.append('0{}/2018'.format(j))
     
 Data = np.array(Data).reshape(5152,1)
 
@@ -132,15 +132,15 @@ serie_municipal.columns = ['Data', 'Município', 'Ano', 'Mês', 'Região', 'Homi
 
 sm_mun_ano = serie_municipal[:]
 sm_mun_ano.index = [sm_mun_ano['Ano']]
-grupo_mun_ano_sm = sm_mun_ano.groupby(sm_mun_ano['Município'])
+grupo_mun_ano_sm = sm_mun_ano.groupby('Município')
 
 sm_ano = serie_municipal[:]
 sm_ano.index = [sm_ano['Mês']]
-grupo_ano_sm = sm_ano.groupby(sm_ano['Ano'])
+grupo_ano_sm = sm_ano.groupby('Ano')
 
 sm_mes = serie_municipal[:]
 sm_mes.index = [sm_mes['Ano']]
-grupo_mes_sm = sm_mes.groupby(sm_mes['Mês'])
+grupo_mes_sm = sm_mes.groupby('Mês')
 
 serie_municipal.index = serie_municipal['Data']
 serie_municipal = serie_municipal.drop('Data', axis=1)
@@ -161,17 +161,17 @@ for i in list(range(2014, 2018)):
     for j in list(range(1, 10)):
         
         for t in list(range(92)):
-            Data.append('{}/0{}'.format(i, j))
+            Data.append('0{}/{}'.format(j, i))
         
     for j in list(range(10, 13)):
         
         for t in list(range(92)):
-            Data.append('{}/{}'.format(i, j))
+            Data.append('{}/{}'.format(j, i))
         
 for j in list(range(1, 9)):
     
     for t in list(range(92)):
-        Data.append('2018/0{}'.format(j))
+        Data.append('0{}/2018'.format(j))
     
 Data = np.array(Data).reshape(5152,1)
 
@@ -190,17 +190,26 @@ ar_mt[boolean_mt] = np.nan
 serie_municipal_taxas = pd.DataFrame(ar_mt)
 serie_municipal_taxas.columns = ['Data', 'Município', 'Ano', 'Mês', 'Região', 'Homicídio Doloso', 'Lesão Corporal seguida de Morte', 'Latrocínio', 'Homicídio por Intervenção Policial', 'Tentativa de Homicídio', 'Lesão Corporal Dolosa', 'Estupro', 'Homicídio Culposo', 'Lesão Corporal Culposa', 'Roubo a Comércio', 'Roubo a Residência', 'Roubo de Veículo', 'Roubo de Carga', 'Roubo a Transeunte', 'Roubo em Coletivo', 'Roubo a Banco', 'Roubo a Caixa Eletrônico', 'Roubo de Celular', 'Roubo com Condução para Saque', 'Roubo a Bicicleta', 'Outros Roubos', 'Total de Roubos', 'Furto de Veículos', 'Furto de Bicicleta', 'Outros Furtos', 'Total de Furtos', 'Sequestro', 'Extorsão', 'Sequestro Relâmpago', 'Estelionato', 'Apreensão de Drogas', 'Recuperação de Veículos', 'Cumprimento de Mandado de Prisão', 'Ameaças', 'Pessoas Desaparecidas', 'Encontro de Cadáver', 'Encontro de Ossada', 'Indicador de Letalidade', 'Indicador de Roubo na Rua', 'Indicador de Roubo de Veículos', 'Registro de Ocorrências']
 
-smt_mun_ano = serie_municipal_taxas[:]
-smt_mun_ano.index = [smt_mun_ano['Ano']]
-grupo_mun_ano_smt = smt_mun_ano.groupby(smt_mun_ano['Município'])
+smt_mun = serie_municipal_taxas.drop(['Data', 'Município', 'Ano', 'Mês', 'Homicídio por Intervenção Policial', 'Total de Roubos', 'Total de Furtos', 'Apreensão de Drogas', 'Recuperação de Veículos', 'Cumprimento de Mandado de Prisão', 'Pessoas Desaparecidas', 'Encontro de Cadáver', 'Encontro de Ossada', 'Indicador de Letalidade', 'Indicador de Roubo na Rua', 'Indicador de Roubo de Veículos', 'Registro de Ocorrências'], axis=1)
+smt_mun = np.array(smt_mun)
+smt_mun = pd.DataFrame(smt_mun, dtype=float)
+smt_mun.columns = ['Região', 'Homicídio Doloso', 'Lesão Corporal seguida de Morte', 'Latrocínio', 'Tentativa de Homicídio', 'Lesão Corporal Dolosa', 'Estupro', 'Homicídio Culposo', 'Lesão Corporal Culposa', 'Roubo a Comércio', 'Roubo a Residência', 'Roubo de Veículo', 'Roubo de Carga', 'Roubo a Transeunte', 'Roubo em Coletivo', 'Roubo a Banco', 'Roubo a Caixa Eletrônico', 'Roubo de Celular', 'Roubo com Condução para Saque', 'Roubo a Bicicleta', 'Outros Roubos', 'Furto de Veículos', 'Furto de Bicicleta', 'Outros Furtos', 'Sequestro', 'Extorsão', 'Sequestro Relâmpago', 'Estelionato', 'Ameaças']
+smt_mun['Município'] = serie_municipal_taxas['Município']
+smt_mun['Data'] = serie_municipal_taxas['Data']
+grupo = smt_mun.groupby('Município')
+grupo_mun_mean = smt_mun.groupby('Município').mean()
 
 smt_ano = serie_municipal_taxas[:]
 smt_ano.index = [smt_ano['Mês']]
-grupo_ano_smt = smt_ano.groupby(smt_ano['Ano'])
+grupo_ano_smt = smt_ano.groupby('Ano')
 
 smt_mes = serie_municipal_taxas[:]
 smt_mes.index = [smt_mes['Ano']]
-grupo_mes_smt = smt_mes.groupby(smt_mes['Mês'])
+grupo_mes_smt = smt_mes.groupby('Mês')
+
+smt_mun_ano = serie_municipal_taxas[:]
+smt_mun_ano.index = [smt_mun_ano['Ano']]
+grupo_mun_ano_smt = smt_mun_ano.groupby(smt_mun_ano['Município'])
 
 serie_municipal_taxas.index = serie_municipal_taxas['Data']
 serie_municipal_taxas = serie_municipal_taxas.drop('Data', axis=1)
